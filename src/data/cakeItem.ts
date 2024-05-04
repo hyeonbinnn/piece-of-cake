e876e88f1197e761878a53a83c97ad21c68c0ce7
hyeonbin jeong <duafuuu@gmail.com>
2024-05-05 01:38:50 +0900

    add: item 추가

--- src/data/cakeItem.ts
+++ src/data/cakeItem.ts
@@ -1,12 +1,21 @@
-import carrotCake from '../assets/cake/carrot-cake.svg';
-import cherryCake from '../assets/cake/cherry-cake.svg';
-import chocoCake from '../assets/cake/choco-cake.svg';
-import pinkCake from '../assets/cake/pink-cake.svg';
-import strawberryCake from '../assets/cake/strawberry-cake.svg';
-import vanillaCake from '../assets/cake/vanilla-cake.svg';
-import darkCake from '../assets/cake/dark-cake.svg';
-import raspberryCake from '../assets/cake/raspberry-cake.svg';
-import saltyCake from '../assets/cake/salty-cake.svg';
+import carrotCake from '../assets/carrot-cake.svg';
+import cherryCake from '../assets/cherry-cake.svg';
+import chocoCake from '../assets/choco-cake.svg';
+import pinkCake from '../assets/pink-cake.svg';
+import strawberryCake from '../assets/strawberry-cake.svg';
+import vanillaCake from '../assets/vanilla-cake.svg';
+import darkCake from '../assets/dark-cake.svg';
+import raspberryCake from '../assets/raspberry-cake.svg';
+import saltyCake from '../assets/salty-cake.svg';
+import rollCake from '../assets/roll-cake.svg';
+import cupCake from '../assets/cup-cake.svg';
+import dessert from '../assets/dessert.svg';
+import donut from '../assets/donut.svg';
+import candy from '../assets/candy.svg';
+import cookie from '../assets/cookie.svg';
+import fruits from '../assets/fruits.svg';
+import milk from '../assets/milk.svg';
+import heartBox from '../assets/heart-box.svg';
 import { CakeItemData } from '../types/data';
 
 const cakeItem: CakeItemData[] = [
@@ -22,21 +31,21 @@ const cakeItem: CakeItemData[] = [
     name: 'cherry cake',
     cost: 6000,
     img: cherryCake,
-    count: 5,
+    count: 6,
   },
   {
     id: 3,
     name: 'choco cake',
     cost: 6500,
     img: chocoCake,
-    count: 5,
+    count: 4,
   },
   {
     id: 4,
     name: 'pink cake',
     cost: 6000,
     img: pinkCake,
-    count: 5,
+    count: 7,
   },
   {
     id: 5,
@@ -50,28 +59,91 @@ const cakeItem: CakeItemData[] = [
     name: 'vanilla cake',
     cost: 7000,
     img: vanillaCake,
-    count: 5,
+    count: 4,
   },
   {
     id: 7,
     name: 'dark cake',
     cost: 6000,
     img: darkCake,
-    count: 5,
+    count: 3,
   },
   {
     id: 8,
     name: 'raspberry cake',
     cost: 6000,
     img: raspberryCake,
-    count: 5,
+    count: 3,
   },
   {
     id: 9,
     name: 'salty cake',
     cost: 7000,
     img: saltyCake,
+    count: 4,
+  },
+  {
+    id: 10,
+    name: 'roll cake',
+    cost: 7000,
+    img: rollCake,
     count: 5,
+  },
+  {
+    id: 11,
+    name: 'cup cake',
+    cost: 5000,
+    img: cupCake,
+    count: 3,
+  },
+  {
+    id: 12,
+    name: 'mini set',
+    cost: 6000,
+    img: dessert,
+    count: 2,
+  },
+  {
+    id: 13,
+    name: 'donut',
+    cost: 5000,
+    img: donut,
+    count: 3,
+  },
+  {
+    id: 14,
+    name: 'candy',
+    cost: 2000,
+    img: candy,
+    count: 4,
+  },
+  {
+    id: 15,
+    name: 'cookie',
+    cost: 3000,
+    img: cookie,
+    count: 6,
+  },
+  {
+    id: 16,
+    name: 'fruits candy',
+    cost: 5000,
+    img: fruits,
+    count: 4,
+  },
+  {
+    id: 17,
+    name: 'milk set',
+    cost: 5000,
+    img: milk,
+    count: 8,
+  },
+  {
+    id: 18,
+    name: 'heart box',
+    cost: 8000,
+    img: heartBox,
+    count: 1,
   },
 ];
 
