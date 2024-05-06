fb1b4795d9c415573c45f2b0e81d3d4a025340a6
hyeonbin jeong <duafuuu@gmail.com>
2024-05-07 00:32:07 +0900

    add: 설명서 모달 기능 추가

---
+++ src/components/Modal/ManualModal.style.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Wrapper = styled.div`
+  background-color: var(--sub-bg);
+  border: 10px solid var(--border);
+  border-radius: 5px;
+  width: 80%;
+  max-width: 800px;
+  height: auto;
+  padding: 40px;
+  position: relative;
+
+  @media (max-width: 767px) {
+    padding: 40px 30px;
+  }
+`;
+
+export const H2 = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 25px;
+`;
+
+export const CloseBtn = styled.button`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+`;
+
+export const Img = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+
+export const ManualUl = styled.ul`
+  line-height: 1.2;
+`;
+
+export const ManualLi = styled.li`
+  margin-bottom: 8px;
+  font-size: 15px;
+  text-align: center;
+
+  &:last-child {
+    font-size: 16px;
+    font-weight: bold;
+    margin: 20px 0px 0px 0px;
+    text-align: center;
+  }
+
+  @media (max-width: 767px) {
+    text-align: left;
+  }
+`;
---
+++ src/components/Modal/ManualModal.tsx
@@ -0,0 +1,39 @@
+import * as S from './ManualModal.style';
+import close from '../../assets/close.svg';
+
+interface OnCloseProps {
+  onClose: () => void;
+}
+
+const ManualModal = ({ onClose }: OnCloseProps) => {
+  const descList: string[] = [
+    '충천액을 입력 후, 충전 버튼을 누르면 소지금이 나타납니다.',
+    '소지금 이하의 금액을 입금액에 입력 후, 입금 버튼을 누르면 잔액이 채워집니다.',
+    '소지금에서 잔액이 차감되고, 원하는 디저트를 잔액 이하로 고를 수 있습니다.',
+    '잔액이 남으면, 거스름돈 반환 버튼을 눌러 소지금을 다시 채울 수 있습니다.',
+    '획득 버튼을 누르면 장바구니에 선택한 디저트와 금액을 최종적으로 확인할 수 있습니다.',
+    '준비 끝! 이제 Piece of Cake 벤딩머신을 즐겨주세요!',
+    '- 매일이 당신의 생일이길 바라는 주인장 👧🏻 올림  -',
+  ];
+
+  return (
+    <S.Container>
+      <S.Wrapper>
+        <S.H2>Piece of Cake 이용 방법</S.H2>
+        <S.CloseBtn onClick={onClose}>
+          <S.Img src={close} alt="닫기 버튼" />
+        </S.CloseBtn>
+
+        <S.ManualUl>
+          {descList.map((desc: string, index: number) => (
+            <S.ManualLi key={index}>
+              {index !== descList.length - 1 ? `${index + 1}.` : ''} {desc}
+            </S.ManualLi>
+          ))}
+        </S.ManualUl>
+      </S.Wrapper>
+    </S.Container>
+  );
+};
+
+export default ManualModal;
