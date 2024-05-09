6982ac261582d324f5606523d0d54c0d6883d3a0
hyeonbin jeong <duafuuu@gmail.com>
2024-05-09 21:10:06 +0900

    add: 타입 확장 및 추가

--- src/state/atoms/atoms.ts
+++ src/state/atoms/atoms.ts
@@ -1,11 +1,30 @@
 import { atom } from 'recoil';
+import { CakeItemData } from '../../types/data';
 
+export interface SelectedCakeItem extends CakeItemData {
+  quantity: number;
+}
+
+// 소지금
 export const amountState = atom<number>({
   key: 'amountState',
   default: 0,
 });
 
+// 잔액
 export const balanceState = atom<number>({
   key: 'balanceState',
   default: 0,
+});
+
+// 선택한 아이템
+export const selectedItemState = atom<SelectedCakeItem[]>({
+  key: 'selectedItemState',
+  default: [],
+});
+
+// 최종 장바구니
+export const totalCartItemState = atom<SelectedCakeItem[]>({
+  key: 'totalCartItemState',
+  default: [],
 });
--- src/state/selectors/selector.ts
+++ src/state/selectors/selector.ts
@@ -1,6 +1,7 @@
 import { selector } from 'recoil';
 import { amountState, balanceState } from '../atoms/atoms';
 
+// 소지금 + 잔액
 export const totalAmountState = selector<number>({
   key: 'totalAmountState',
   get: ({ get }): number => {
