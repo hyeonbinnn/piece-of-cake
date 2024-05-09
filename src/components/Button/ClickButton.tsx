0250d9c52ec62debdbc864e289a9bf4175ea6b9a
hyeonbin jeong <duafuuu@gmail.com>
2024-05-10 00:38:22 +0900

    add: 입금, 거스름돈 반환, 충전 버튼

---
+++ src/components/Button/ClickButton.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+type ButtonProps = {
+  text: string;
+  className?: string;
+  onClick?: () => void;
+};
+
+const ClickButton = ({ text, className, onClick }: ButtonProps) => {
+  return (
+    <CommonBtn type="button" className={className} onClick={onClick}>
+      {text}
+    </CommonBtn>
+  );
+};
+
+export default ClickButton;
+
+export const CommonBtn = styled.button`
+  box-shadow: 0px 0px 4px var(--box);
+  border-radius: 5px;
+  font-size: 13px;
+  font-weight: bold;
+`;
